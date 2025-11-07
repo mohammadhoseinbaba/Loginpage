a494f5b44ffce86ccc03eed704c952b76ae752d8
mohammadhoseinbab <sirhb77@gmail.com>
2025-11-07 15:59:35 +0100

    add css to signup

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -31,19 +31,39 @@ function SignUp({ onLoginShow, onSign }) {
         setName(e.target.value)
     }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Enter your Name </label>
-                <input value={name} onChange={handleName} type='text' />
-                <br />
-                <label>Enter your email address </label>
-                <input value={email} onChange={handleEmail} type='email' />
-                <br />
-                <label>Enter your Password </label>
-                <input value={password} onChange={handlePassword} type='password' />
-                <br />
-                <button type='submit'>Sign up</button>
-                <button onClick={handleLogin} type='button'>Log in</button>
+        <div className='min-h-screen grid place-items-center px-4'>
+            <form className='w-full max-w-sm' onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='name'>Name </label>
+                    <input
+                        className='mt-3 block min-w-0 bg-gray-500 rounded pr-3 pl-1 text-base text-white placeholder:text-gray focus:outline-none'
+                        id='name'
+                        value={name}
+                        onChange={handleName}
+                        type='text' />
+                </div>
+                <div className='mt-3'>
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        className='mt-3 block min-w-0 bg-gray-500 rounded pr-3 pl-1 text-base text-white placeholder:text-gray focus:outline-none'
+                        required
+                        id='email'
+                        value={email}
+                        onChange={handleEmail}
+                        type='email' />
+                </div>
+                <div className='mt-3 mb-3'>
+                    <label htmlFor='password'>Password </label>
+                    <input
+                        className='mt-3 block min-w-0 bg-gray-500 rounded pr-3 pl-1 text-base text-white placeholder:text-gray focus:outline-none'
+                        required
+                        id='password'
+                        value={password}
+                        onChange={handlePassword}
+                        type='password' />
+                </div>
+                <button className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700 mt-3 " type='submit'>Sign up</button>
+                <button className="bg-violet-600 text-white px-4 py-2 rounded hover:bg-violet-700 ml-3" onClick={handleLogin} type='button'>Log in</button>
             </form>
 
         </div>
